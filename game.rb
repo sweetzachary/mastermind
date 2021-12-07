8b411bd6be309f3074b35146d7dca556d5f879a5
sweetzachary <a.ntigr16@gmail.com>
2021-12-07 15:42:51 +0200

    Outline game flow in the Game class

---
+++ game.rb
@@ -0,0 +1,39 @@
+class Game
+  def initialize
+    @code = Code.new
+    @turns = 12
+  end
+
+  def run
+    until finished?
+      read
+      asess
+      display
+    end
+  end
+
+  private
+
+  def read
+    puts 'Enter a guess consisting of 4 numbers from 1 to 6 each.'
+    @guess = Guess.new(gets(4))
+  end
+
+  def asess
+    @guess_rating = @code.asess(guess)
+  end
+
+  def display
+    puts "Your guess rating #{@guess_rating}"
+    puts "#{@turns} turns remaining."
+  end
+
+  def finished?
+    if @guess_rating.correct?
+      puts 'YOU WON!'
+    elsif turns <= 0
+      puts 'YOU LOST'
+    else
+      @turns -= 1
+    end
+end
